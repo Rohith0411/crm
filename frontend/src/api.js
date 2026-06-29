4ca6530a445dd7395f495cc9e15ff9412c1ace06
Rohith A <rohithar0411@gmail.com>
2026-06-29 13:24:33 +0530

    Update API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"; //call api
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", //dont want to write full url
+  baseURL: "https://crm-fswr.onrender.com", //dont want to write full url
 });
 
 API.interceptors.request.use((req) => {

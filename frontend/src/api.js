3c41e47345d2c4ef7ce7a55195710a46f5050940
Rohith A <rohithar0411@gmail.com>
2026-06-29 13:37:41 +0530

    Connect frontend to deployed backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"; //call api
 
 const API = axios.create({
-  baseURL: "https://crm-fswr.onrender.com", //dont want to write full url
+  baseURL: "https://mini-crm-backend-o2iw.onrender.com", //dont want to write full url
 });
 
 API.interceptors.request.use((req) => {
